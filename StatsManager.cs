be40f434e8a61806e09ade0b6670b3c764eed5bc
Tom Nguyen <snguyet773@gmail.com>
2025-04-01 14:44:02 -0500

    added comments to StatsManager

--- StatsManager.cs
+++ StatsManager.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine.SceneManagement;
 
+
+/**
+ * @brief A class to hold the multiple instances of statistics that are saved/will be saved in a JSON file through lists
+ * 
+ */
 [System.Serializable]
 public class StatsData
 {
@@ -11,7 +16,7 @@ public class StatsData
 }
 
 /**
- * testing javadoc
+ * @brief A class that manages user stats taken from data acquired in the simulation scene
  *
  */
 public class StatsManager : MonoBehaviour
@@ -23,6 +28,11 @@ public class StatsManager : MonoBehaviour
 
 	public float currentElapsedTime;
 
+
+    /**
+     * @brief On awake mark the object to persist and keep track of the file path, then load the stats from the file
+     * 
+     */
     void Awake() {
         DontDestroyOnLoad(gameObject);
         Debug.Log("StatsManager has been initialized.");
@@ -31,6 +41,12 @@ public class StatsManager : MonoBehaviour
         LoadStats();
     }
 
+    /**
+     * @brief Stores an instance of stats from a user's session and then calls the SaveStats() method to save them
+     * 
+     * @param playerName
+     * @param elapsedTime a float value representing the user's time taken from the current session
+     */
     public void AddRecord(string playerName, float elapsedTime)
     {
         statsData.playerNames.Add(playerName);
@@ -38,24 +54,63 @@ public class StatsManager : MonoBehaviour
         SaveStats();
     }
 
+    /**
+     * @brief Sets the name of the user
+     * 
+     * @param n a string that represents the name of the user
+     * 
+     * @usage
+     * setName("Bob"); // currentName will be "Bob"
+     */
     public void setName(string n) {
 		currentName = n;
 	}
 
-	public string getName() {
+    /**
+     * @brief Gets the name set by the user
+     * 
+     * @return The name of the user
+     * 
+     * @usage
+     * String name = getName(); // name will be the value of currentName
+     */
+    public string getName() {
 		return currentName;
 	}
 
+
+    /**
+     * @brief Sets the elapsed time as a float value
+     * 
+     * @param time a float that represents the time taken by the user
+     * 
+     * @usage
+     * setElapsedTime(12.0); // currentElapsedTime stat will be 12.0
+     */
     public void setElapsedTime(float time)
     {
         currentElapsedTime = time;
     }
 
+
+    /**
+     * @brief Gets the time taken by the user from the simulation scene
+     * 
+     * @return The time taken in the simulation scene
+     * 
+     * @usage
+     * float timeTaken = getElapsedTime(); // timeTaken will be the value of the currentElapsedTime
+     */
     public float getElapsedTime()
     {
         return currentElapsedTime;
     }
 
+    /**
+     * @brief The initial method for keeping track of the name and time of the user called when the user inputs their name
+     * 
+     * 
+     */
     public void SaveCurrentRecord()
     {
         if (!string.IsNullOrEmpty(currentName))
@@ -66,16 +121,31 @@ public class StatsManager : MonoBehaviour
         }
     }
 
+    /**
+     * @brief Retrieve the list of player names stored in the statsData class
+     * 
+     * @return a list of player names
+     *
+     */
     public List<string> GetPlayerNames()
     {
         return statsData.playerNames;
     }
 
+    /**
+     * @brief retrieve the list of time taken in the simulation stored in the statsData class
+     * 
+     * @return a list of times
+     */
     public List<float> GetTimes()
     {
         return statsData.times;
     }
 
+    /**
+     * @brief a method that saves an instance of stats saved in the statsData class to the JSON file
+     * 
+     */
     private void SaveStats()
     {
         string json = JsonUtility.ToJson(statsData, true);
@@ -83,6 +153,11 @@ public class StatsManager : MonoBehaviour
         Debug.Log("Saved Stats: " + json);
     }
 
+    /**
+     * @brief a method that reads the stats file and loads them into the respective variables when called
+     * 
+     * 
+     */
     private void LoadStats()
     {
         if (File.Exists(filePath))
